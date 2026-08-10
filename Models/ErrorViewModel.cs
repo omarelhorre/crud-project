2036bc84336992f0a590a729b52e411b48617e80
OmarDESK <omarelhorre20@icloud.com>
2026-08-10 17:21:50 +0100

    Initialized C# project

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace stagiaireCRUD.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
